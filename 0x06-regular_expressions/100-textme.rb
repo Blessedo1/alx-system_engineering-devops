8c43efaf6f0a8578ad8316a0b580a04573ee82e2
Blessed Adesina <adesinablessed19@gmail.com>
2024-11-08 23:54:56 +0100

    code updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 from = ARGV[0].scan(/from:(.*?)\]/)
 to = ARGV[0].scan(/to:(.*?)\]/)
 flags = ARGV[0].scan(/flags:(.*?)\]/)
-puts [from, to, flags].join(',')
+puts [from.flatten, to.flatten, flags.flatten].join(',')
