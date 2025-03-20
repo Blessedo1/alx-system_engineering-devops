553020f756bdb33ef3f14bf9fe2be677ff45653b
Blessed Adesina <adesinablessed19@gmail.com>
2025-03-20 17:27:19 +0100

    code added

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Returns to-do list information for all employees and exports data to JSON."""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    todos = requests.get(url + "todos").json()
+
+    for i in users:
+        user_id = i.get("id")
+        username = i.get("username")
+        all_tasks[user_id] = []
+        
+        for j in todos:
+            if j.get("userId") == user_id:
+                all_tasks[user_id].append({
+                    "username": username,
+                    "task": j.get("title"),
+                    "completed": j.get("completed")
+                    })
+
+    with open(todo_all_employees.json, mode='w') as file:
+        json.dump(all_tasks, file)
