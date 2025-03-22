cabc3deaedbbbf02d61accd73e64985590158374
Blessed Adesina <adesinablessed19@gmail.com>
2025-03-22 13:03:16 +0100

    code added

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains top_ten function"""
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the first 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "custom"}
+    params = {"limit": 10}
+
+    r = reuests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if r.status_code == 200:
+        data = r.json()
+        posts = data.get("data", {}).get("children", 0)
+        for i in posts:
+            print(i["data"]["title"])
+    else:
+        print(None)
